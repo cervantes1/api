4ccea2d955a030fcd31a05626e9606b50b5205b1
cervantes1 <35356283+cervantes1@users.noreply.github.com>
2021-04-07 16:06:09 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+import pandas as pd
+import json
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+api = Api(app)
+
+@app.route("/")
+def hello():
+    return "Property API"
+
+@app.route('/properties/<record>', methods=['GET'])
+def get_records(record):
+    with open('../flask/Data/data.json') as f:
+        data = json.load(f)
+
+    if record in data:
+        return {record : data[record]}, 200
+
+class properties(Resource):
+    def get(self):
+        with open('../flask/Data/data.json') as f:
+            data = json.load(f)
+
+        return {'Properties': data}, 200
+
+
+api.add_resource(properties, '/properties')
+
+if __name__ == "__main__": 
+    app.run()
+ 
